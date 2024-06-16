2f4c4292614259932a7c2f4e49e5ffe07113641c
julie BRUNETTO <julie.brunetto@octo.com>
2024-06-16 09:08:24 +0200

    feat(config): ajout metamask en connector et config polygonAmoy

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,18 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+import { createConfig, http } from 'wagmi'
+import { polygonAmoy } from 'wagmi/chains'
+import { metaMask } from 'wagmi/connectors'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
-  connectors: [
-    injected(),
-    coinbaseWallet({ appName: 'Create Wagmi' }),
-    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
-  ],
-  ssr: true,
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
+	chains: [polygonAmoy],
+	ssr: true,
+	connectors: [metaMask()],
+	transports: {
+		[polygonAmoy.id]: http(),
+	},
 })
 
 declare module 'wagmi' {
-  interface Register {
-    config: typeof config
-  }
+	interface Register {
+		config: typeof config
+	}
 }
